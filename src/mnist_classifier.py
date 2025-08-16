95700238b4dcfaf29b5b95a04fd3202b67a200ea
siwa-p <prasiwakoti@gmail.com>
2025-08-16 00:02:31 -0400

    more changes

--- src/mnist_classifier.py
+++ src/mnist_classifier.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
-from torchinfo import summary
 import optuna
 from utilities.utils import MLPModel  
 mlflow.set_tracking_uri("http://localhost:5000")
